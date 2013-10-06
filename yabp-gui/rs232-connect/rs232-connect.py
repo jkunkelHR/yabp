86c4b09fad61c1d20e971bf6cba5b43e73985c63
jkunkel <jochen.kunkel@gmail.com>
2013-10-06 14:39:01 +0200

    first-checkout

---
+++ yabp-gui/rs232-connect/rs232-connect.py
@@ -0,0 +1,17 @@
+'''
+Created on Oct 6, 2013
+
+@author: jkunkel
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(selfparams):
+        '''
+        Constructor
+        '''
+        
